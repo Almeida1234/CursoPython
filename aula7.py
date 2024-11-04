7ca192a76c1360971608ebe5f5659539b0b31c25
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-04 17:57:34 +0000

    Introdução às variáveis em Python

--- aula7.py
+++ aula7.py
@@ -7,12 +7,12 @@
 
 # nome_completo = 'Luiz Otávio Miranda'
 # soma_dois_mais_dois = 2 + 2
-# int_um = bool('1')
+# int_um = bool ('1')
 # print(int_um, type(int_um))
-# print(nome_completo, soma_dois_mais_dois)
+# print(nome_completo,'\n',soma_dois_mais_dois)
 
 nome = 'Luiz'
-idade = 17
+idade = 36
 maior_de_idade = idade >= 18
-print('Nome:', nome, 'Idade:', idade)
+print('Nome:', nome,'\n''Idade:', idade)
 print('É maior?', maior_de_idade)
