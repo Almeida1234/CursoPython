c1f23dd766cd780afa729a3d30516113790977da
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-04 12:32:30 +0000

    Coerção de tipos (Converter um tipo em outro

--- aula6.py
+++ aula6.py
@@ -5,7 +5,7 @@
 # str, int, float, bool
 print(1 + 1) #soma, pois são números Inteiros (int)
 print( 'a' + 'b') #concatena (Junta), pois são caracteres (str)
-print(type(int('1')), type(int('1')))
+print(int('1'), type(int('1')))
 print(type('1'), type(int('1'))) #quando incluimos o int, string, float na frente ele faz a conversão
 print(int('1') + 1)
 print(float('1') + 1)
