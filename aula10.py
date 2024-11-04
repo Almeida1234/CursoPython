ee859d845b10fc7ca04fb88d64fdef60ebc24ac9
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-04 23:34:02 +0000

    Concatenação (+) e repetição (*) com operadores aritméticos

---
+++ aula10.py
@@ -0,0 +1,8 @@
+# concatenacao = 'A' + 'B' + 'C'
+concatenacao = 'Ricardo' + ' ' + 'Cecilia'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
