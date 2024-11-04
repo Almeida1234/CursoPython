37835d5604e311c2eb7519e8bda3d2e2b6600c15
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-04 18:25:51 +0000

    Exercício com variáveis e tipos de dados (solucionado)

---
+++ aula8.py
@@ -0,0 +1,18 @@
+# nome = str('Ricardo')
+# sobrenome = str('Almeida')
+# idade = int(17)
+# ano_nascimento = int(1987)
+# maior_de_idade =  idade >= bool(18)
+# altura_metros = float(1.74)
+nome = 'Ricardo'
+sobrenome = 'Almeida'
+idade = 37
+ano_nascimento = 2024 - idade
+maior_de_idade =  idade >= 18
+altura_metros = 1.74
+print('Nome:', type(nome), nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
