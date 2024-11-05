1c24f951ad70c7370d02128820ed87e47df28ef1
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-05 00:12:51 +0000

    Solução exercício de programação - Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Ricardo Almeida'
+altura = 1.74
+peso = 95
+imc = peso / (altura * altura)
+#print(nome, altura, peso, imc)
+print('Meu nome:', nome,'\n', 'Minha Altura:', altura,'\n', 
+      'Meu peso:', peso,'Kl','\n', 'Minha média caorporal:', imc)
+
+# Ricardo Almeida tem 1.74 de altura,
+# pesa 95 quilos e seu IMC é 
+# 31.37798916633637
