5e019be4059f7d7e6233718ce6e2ee6eea9f524b
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-06 12:51:58 +0000

    Introdução aos blocos de código + if / elif / else (condicionais)

---
+++ aula16.py
@@ -0,0 +1,15 @@
+# if / elif       / else
+# se / se não se  / se não
+
+entrada = input('Você quer "Entrar" ou "Sair" ')
+
+if entrada == 'Entrar' or entrada == 'entrar':
+    print('Você entrou no sistema')
+
+elif entrada == ('Sair'):
+    print('Você saiu do sistema')
+
+else:
+    print('Você precisa escolher entar ou sair')
+
+print('Obrigado!!!')
