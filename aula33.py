997a175ec416ad9d5a93f4d18ddff51859625842
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-09 01:15:20 +0000

    Documentação

---
+++ aula33.py
@@ -0,0 +1,10 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Ricardo'
+outra_variavel = f'{string[:7]}ABC{string[8:]}'
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+print(string.zfill(10))
