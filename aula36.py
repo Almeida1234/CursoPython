a0bc959f92ae8cf4d5561c05d1c76038da6f0bfe
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-10 12:33:31 +0000

    Operadores de atribuição com operadores aritméticos

---
+++ aula36.py
@@ -0,0 +1,16 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+
+# while contador <= 10:
+#     contador = contador + 1
+#     print(f'o contador está no numero: {contador}')
+
+# print('Acabou')
+
+#contador = contador + 1
+contador //= 10 # é a mesma coisa que (Contador = contador + 1)
+print(contador)
+
