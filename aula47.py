a86e68e5a9c6725f37130d32458b1aa518d3c88c
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-20 01:39:55 +0000

    (Parte 2) Solução do exercício - Jogo da palavra secreta

--- aula47.py
+++ aula47.py
@@ -16,8 +16,7 @@ usuário.
 """
 
 import os
-
-#Meu código
+#Meu código com for
 palavra_secreta = 'Cecilia'.lower()
 letra_certa = ''
 letra_errada = ''
@@ -54,3 +53,45 @@ print(f'"PARABÉNSA"' \
       f'A palavra secreta é: "{palavra_secreta}" você acertou!\n' \
       f'você tentou: {contador}X')
 print(f'Essas são todoas as Letras Erradas que você digitou: " {letra_errada}"')
+
+# Meu código sem for só com while
+# palavra_secreta = 'Cecilia'.lower()
+# letra_certa = ''
+# letra_errada = ''
+# contador = 0
+# palavra_formada = ''
+
+# # Loop principal
+# while palavra_formada != palavra_secreta:
+#     digite_letra = input('Digite uma letra: ').lower()
+#     letra = digite_letra
+    
+#     if len(letra) > 1:
+#         print('Você digitou mais de uma letra.')
+#         continue
+
+#     if letra in palavra_secreta:
+#         letra_certa += letra
+#         contador += 1
+#     else:
+#         letra_errada += f'{letra}, '
+#         contador += 1
+
+#     palavra_formada = ''
+#     # Substituir o laço 'for' por 'while' para construir a palavra formada
+#     index = 0
+#     while index < len(palavra_secreta):
+#         letra_secreta = palavra_secreta[index]
+#         if letra_secreta in letra_certa:
+#             palavra_formada += letra_secreta
+#         else:
+#             palavra_formada += '*'
+#         index += 1
+
+#     print(f'Palavra formada: {palavra_formada}')
+
+# # Limpar a tela (caso esteja em um terminal que suporte)
+# os.system('clear')
+# print(f'"PARABÉNS!" A palavra secreta é: "{palavra_secreta}" você acertou!\n' \
+#       f'Você tentou: {contador}X')
+# print(f'Essas são todas as letras erradas que você digitou: "{letra_errada}"')
