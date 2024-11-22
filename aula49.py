4df9ce965edb29561acbb9f0b772df6f03224a92
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-22 19:37:00 +0000

    for in com list

---
+++ aula49.py
@@ -0,0 +1,16 @@
+"""
+for in com listas
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+
+for nome in lista:
+    print(nome, type(nome))
+
+
+
+# indice = 0
+# while indice < len(lista):
+#     nome1 = lista[indice]
+#     print(nome1, type(nome1))
+#     indice += 1
+
