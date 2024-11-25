83e95d691255dce23d7398065472aac7143fcf60
Almeida1234 <almeidasilva_rick@hotmail.com>
2024-11-25 20:11:26 +0000

    Tabuada

---
+++ aula55_1.py
@@ -0,0 +1,20 @@
+#tabuada
+
+i = 1
+while i <= 10:
+    print(f'Tabuada do {i}:')
+    
+    j = 1
+    while j <= 10:
+        print(f'{i} x {j} = {i * j}')
+        j += 1
+    print()
+    i += 1
+
+
+# for i in range(1, 11):
+#     print(f'Tabuada do {i}: ')
+
+#     for j in range(1,11):
+#         print(f'{i} x {j} = {i * j}')
+#     print()
